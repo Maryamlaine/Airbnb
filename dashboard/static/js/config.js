25e02fe546aebf02f0a58ecbfd9b9d32f23a51f4
AndrewMac34 <60302659+AndrewMac34@users.noreply.github.com>
2020-07-16 19:12:37 -0500

    Add files via upload
    Adding updated javascript files for map

--- dashboard/static/js/config.js
+++ dashboard/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWFyeWFtbGFpbmUiLCJhIjoiY2thZWtkaDBsMDFrajJ3bm9oYjZndXFmaiJ9.wtYCwL2EJTH6BAm-jxRaYA";
+const API_KEY = "pk.eyJ1IjoibWNnNzUzMiIsImEiOiJja2Fla2lqaHAyaWU4MnlwbXMzbGhybDlvIn0.fSfRis-NHogtedCNrgKKyA"
